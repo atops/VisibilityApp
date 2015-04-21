648ed5381adb2133eb3f3beb773d09d336d7330a
atops <alan.toppen@gmail.com>
2015-04-21 07:55:37 -0400

    Updated comments

--- server.R
+++ server.R
@@ -16,21 +16,23 @@ df <- read.csv("data/2013 Visibility Data.csv",
                na.strings="#N/A")
 
 # Use dplyr and tidyr packages to tidy the data and prepare for plotting.
-#  Remove fields we don't need.
-#  Convert to date/time data type.
-#  "unpivot" (gather) MM.X.X columns into "Visibility" column
-# Save as new data.frame in case want to go back to original csv
 df <- df %>% 
+        # Remove fields we don't need.
         select(-(Month:Serial..)) %>%
+        # blank dates = bad data
         filter(Date...Time!="") %>% 
+        # Convert to date/time data type.
         mutate(Date...Time = as.POSIXct(Date...Time, format="%m/%d/%Y %H:%M")) %>%
+        # "unpivot" (gather) MM.X.X columns into "Visibility" column
         gather(MileMarker, Visibility, MM.1.2:MM.16.9) %>%
+        # convert feet to miles
         mutate(Visibility_mi = Visibility/5280)
 
 milemarkers <- levels(df$MileMarker)
 
 shinyServer(function(input, output, session) {
         
+        # Allow the user to select the ESS they want.
         observe({
                 updateCheckboxGroupInput(session, "stations",
                           choices = milemarkers,
@@ -40,8 +42,8 @@ shinyServer(function(input, output, session) {
         output$timeseries <- renderPlot({
 
                 # Subset by time period and facet by MileMarker
-                t1 <- strftime(input$dates[1], "%Y-%m-%d") # inclusive
-                t2 <- strftime(input$dates[2] + days(1), "%Y-%m-%d") # inclusive of 
+                t1 <- strftime(input$dates[1], "%Y-%m-%d") # from 12:00 am
+                t2 <- strftime(input$dates[2] + days(1), "%Y-%m-%d") # until 12:00 am next day
                 
                 df2 <- df %>% 
                         filter(Date...Time >= t1 & Date...Time < t2) %>%
@@ -57,6 +59,6 @@ shinyServer(function(input, output, session) {
                         theme(axis.text.x = element_text(size=8)) +
                         theme(axis.text.y = element_text(size=8))
 
-        }, height=800)
+        }, height=800) # height defaults to 400; can't seem to make it fill the screen automatically
 
 })
--- ui.R
+++ ui.R
@@ -15,26 +15,27 @@ shinyUI(fluidPage(
         # Sidebar with a slider input for number of bins
         sidebarLayout(
                 sidebarPanel(
-                        h3("2013 Data"),
-                        dateRangeInput("dates",
-                                "Date Range:",
+                        dateRangeInput("dates", "Select Date Range:",
                                 start = "2013-07-01",
-                                end = "2013-07-07",
+                                end = "2013-07-03",
                                 min = "2013-01-01",
                                 max = "2013-12-31",
                                 format = "mm/dd/yyyy"),
-                        checkboxGroupInput("stations",
-                                "ESS:",
+                        checkboxGroupInput("stations", "Select ESS:",
+                                # choices are hardcoded here to save a screen refresh, but they 
+                                # are dynamically updated right off the bat based on the input data.
                                 choices = c("MM.1.2","MM.1.8","MM.2.7","MM.3.05","MM.4.4","MM.5.3","MM.6.6","MM.7.3","MM.9.0","MM.9.6","MM.11.3","MM.16.9")),
-                                #multiple = TRUE),
+                        # Some explanation for the user
                         p("This plot shows visibility data from Environmental Sensor Stations (ESS) on a major freeway in the United States that is often subject to foggy conditions, which reduces visibility and which has contributed  to too many severe accidents.\n"),
                         p("The location of each ESS is denoted by Mile Marker (MM), from 1.2 to 16.9; each station is arranged in order vertically according to its location.\n"),
                         p("When visibility is not impeded, the stations return a value of 6562 ft, which on the plot is the flatline at approx. 1.2 mi."),
                         p("Data from the 2013 calendar year was made available for analysis. Each point is a reading taken at 10 minute intervals."),
+                        # sidebar width defaults to 4 (out of 12 units wide). 
+                        # make it narrower to allow more room for the plot.
                         width = 3
                 ),
 
-                # Show a plot of the generated distribution
+                # Show a plot of the selected data
                 mainPanel(
                         plotOutput("timeseries")
                 )
